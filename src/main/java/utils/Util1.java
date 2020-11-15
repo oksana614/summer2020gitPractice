252453963059f05c71b72a99b0b3c153642ae588
oksanafdrv <oksanafdrv@gmail.com>
2020-11-14 18:19:02 -0600

    Add Search functionality to Util1 class

--- src/main/java/utils/Util1.java
+++ src/main/java/utils/Util1.java
@@ -4,4 +4,8 @@ public class Util1 {
     public static void login(String username, String password) {
         System.out.println("logging in using: "+username+", pswd: "+password);
     }
+
+    public static void searchFor(String searchVal){
+        System.out.println("Searching For: "+searchVal);
+    }
 }
